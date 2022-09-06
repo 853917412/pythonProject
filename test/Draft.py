b1bff61ec8315feee726bf33cbcac7762bacbf5e
yihao.sun <853917412@qq.com>
2022-09-06 18:30:53 +0800

    1

--- test/Draft.py
+++ test/Draft.py
@@ -1,19 +1,12 @@
 # x = (2, 1, 0)
 # print("".join('%s' %id for id in x))
 
-
-# x = [3,'a', 'b', 'c',True,True,False]
-# for i in range(len(x)):
-#     if type(x[i]) == bool and x[i] == True:
-#         print(i)
+from typing import List
 
 
 
 
 
+import 每日一题
 
-
-li = [[1,0,0],
-      [0,0,1],
-      [1,0,0]]
-p_1582(li)
+每日一题.p_1582([[1,0,0],[0,1,0],[0,0,1]])
