e30945bb79c50ee6a2b83000877a9aca60c500a5
yihao.sun <853917412@qq.com>
2023-05-22 18:05:38 +0800

    1

--- test/Draft.py
+++ test/Draft.py
@@ -1,1 +1,15 @@
-open()
+
+
+# File = open(file = "File.txt",mode="r",encoding='utf-8')
+# note = File.readlines()
+# print(note)
+# File.close()
+x = 0
+with open(file = 'File.txt',mode = 'a+',encoding='utf-8') as File:
+    File.write(f'\ntest{x}')
+    File.flush()
+    File.seek(0,0)
+    content = File.read()
+    print(content)
+
+
